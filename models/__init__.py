f8ed21d982b3238a3173ef52f50ae6e3c30ae28d
Sébastien Bühl <buhlsebastien@gmail.com>
2022-01-31 13:50:27 +0200

    [FIX] fields

--- models/__init__.py
+++ models/__init__.py
@@ -4,4 +4,5 @@ from . import crm_lead
 from . import sale_order
 from . import account_move
 from . import purchase_order
+
 from . import project
--- models/project.py
+++ models/project.py
@@ -7,6 +7,6 @@ class Tasks(models.Model):
     _name = "project.task"
     _inherit = "project.task"
 
-    
+    sale_order = fields.Many2one("sale.order")
     purchase_order = fields.Many2one("purchase.order")
     purchase_requisition = fields.Many2one("purchase.requisition", string="Call For Tender")

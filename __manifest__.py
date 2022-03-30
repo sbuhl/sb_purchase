c4387f79501754676b680bbf0fb4f97a13d47a49
Sébastien Bühl <buhlsebastien@gmail.com>
2022-03-30 11:03:38 +0200

    [FIX] new product template id + new V

--- __manifest__.py
+++ __manifest__.py
@@ -13,7 +13,7 @@
     'website': "http://www.buhl.be",
 
     'category': 'Customizations',
-    'version': '0.1.2',
+    'version': '0.1.3',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly

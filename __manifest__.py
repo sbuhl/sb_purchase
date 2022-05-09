0f72f81414ddf957c17d0846fcd5d6d112f214c2
Sébastien Bühl <buhlsebastien@gmail.com>
2022-05-09 08:56:14 +0200

    [IMP] project: Display Parent Task even if not in technical mode

--- __manifest__.py
+++ __manifest__.py
@@ -13,7 +13,7 @@
     'website': "http://www.buhl.be",
 
     'category': 'Customizations',
-    'version': '0.1.3',
+    'version': '0.1.4',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly

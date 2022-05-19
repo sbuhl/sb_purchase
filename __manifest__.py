d57fbc92ab8047d7e880ca3f783c6c64744283e9
Sébastien Bühl <buhlsebastien@gmail.com>
2022-05-19 11:43:13 +0200

    [IMP] Remove group on Sale order, to be visible by everyone. Todo: Remove All Document from Héritier et Eric

--- __manifest__.py
+++ __manifest__.py
@@ -13,7 +13,7 @@
     'website': "http://www.buhl.be",
 
     'category': 'Customizations',
-    'version': '0.1.4',
+    'version': '0.1.5',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly

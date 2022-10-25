b386a69c2511c52e07479a2f9dcaec6675f3af02
Sébastien Bühl <buhlsebastien@gmail.com>
2022-10-25 07:48:37 +0200

    [FIX] gse_custo: wrong ID on report

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     'website': "http://www.buhl.be",
 
     'category': 'Customizations',
-    'version': '0.1.7.6',
+    'version': '0.1.7.7',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly

04d0b77cb57c545c3ee32d3d268faa65e3354867
Sébastien Bühl <buhlsebastien@gmail.com>
2022-11-29 16:08:27 +0200

    [IMP] gse_custo: display Project Updates for regular user

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     'website': "http://www.buhl.be",
 
     'category': 'Customizations',
-    'version': '0.1.7.9',
+    'version': '0.1.8.1',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly

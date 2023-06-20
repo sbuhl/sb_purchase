cb1522f41d4d823d16fe7c6ac895d59445fa234e
Sébastien Bühl <buhlsebastien@gmail.com>
2023-06-20 10:34:15 +0200

    [ADD] Expected Closing on CRM

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     'website': "http://www.buhl.be",
 
     'category': 'Customizations',
-    'version': '0.1.8.5',
+    'version': '0.1.8.6',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly

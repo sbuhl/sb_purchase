c32187bac3b752c574362e31e71022450827931b
shortcut1337 <really_mad@hotmail.com>
2023-06-27 19:29:09 +0200

    [IMP] Ajout d'un champ lié entre SO et factures.
    To consider:
    - un SO peut avoir plusieurs factures
    - plusieurs SO peuvent avoir une seule facture.
    
    Spec:
    - Cas 1: Si 1 SO et 1 Invoice
      => Si je coche d'un côté, elle est cochée de l'autre.
    - Cas 2: Si 1 SO et +sieurs Invoices
      Si je coche sur la SO, cela doit être appliqué à toutes les factures
      Si je coche sur une facture, ça doit être appliqué sur la SO & sur
      les autres factures
    - Cas 3: Si +sieurs SO et +sieurs Invoices
      Si je coche sur une SO, cela doit être appliqué sur la/les factures &
      sur le/les autres SO
      Si je coche sur une facture, cela doit être appliqué sur la/les
      factures & sur le/les autres SO

--- models/account_move.py
+++ models/account_move.py
@@ -9,30 +9,22 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     bon_commande = fields.Binary()
-
     bon_livraison = fields.Binary()
-
     paiement_filename = fields.Binary()
-
     date_rappel_manuel = fields.Date()
-
     preuve_paiement = fields.Boolean()
-
     dossier_complet = fields.Boolean()
-
     travaux_termines = fields.Boolean()
-
     salesperson_eval = fields.Boolean(
         string="Exclure de l'évaluation du vendeur",
         tracking=True,
     )
-
     salesperson_eval_reason = fields.Char(
-        string='Raison',
-        tracking=True
+        string='Raison', tracking=True,
     )
-    
     derniere_date_paiement = fields.Date(compute='_compute_derniere_date_paiement')
+
+    sbu_field = fields.Boolean('TODO SBU FIELD')
 
     def _compute_derniere_date_paiement(self):
         for move in self:
@@ -41,12 +33,23 @@ class AccountMove(models.Model):
                 if not last_date or payment['date'] > last_date:
                     last_date = payment['date']
             move.derniere_date_paiement = last_date
-            
+
     @api.returns('self', lambda value: value.id)
     def copy(self, default=None):
         if not self.env.user.has_group('account.group_account_manager'):
             raise UserError("La copie des facture a été désactivée.")
         return super().copy(default)
+
+    def write(self, values):
+        res = super().write(values)
+
+        if 'sbu_field' in values and not self._context.get('ignore_sbu_field'):
+            orders, invoices = self.line_ids.sale_line_ids.order_id.get_recursively_not_directly_related()
+            orders.with_context(ignore_sbu_field=True).sbu_field = values['sbu_field']
+            invoices.with_context(ignore_sbu_field=True).sbu_field = values['sbu_field']
+
+        return res
+
 
 class AccountBankStatement(models.Model):
     _inherit = ['account.bank.statement', 'mail.activity.mixin']
--- models/sale_order.py
+++ models/sale_order.py
@@ -9,3 +9,30 @@ class SaleOrder(models.Model):
 
     # add tracking to this existing field
     referrer_id = fields.Many2one(tracking=True)
+
+    sbu_field = fields.Boolean('TODO SBU FIELD')
+
+    def write(self, values):
+        res = super().write(values)
+
+        if 'sbu_field' in values and not self._context.get('ignore_sbu_field'):
+            orders, invoices = self.get_recursively_not_directly_related()
+            orders.with_context(ignore_sbu_field=True).sbu_field = values['sbu_field']
+            invoices.with_context(ignore_sbu_field=True).sbu_field = values['sbu_field']
+
+        return res
+
+    def get_recursively_not_directly_related(self, all_orders=None, all_invoices=None):
+        # Care that it could auto discover one that has already been
+        # discovered and enter infinite loop
+        all_invoices = all_invoices or self.env['account.move']
+        all_orders = all_orders or self.env['sale.order']
+        all_orders |= self
+        for order in self:
+            related_invoices = order.order_line.invoice_lines.move_id
+            related_orders = related_invoices.line_ids.sale_line_ids.order_id
+            new_discovered_orders = related_orders.filtered(lambda o: o not in all_orders)
+            oo, ii = new_discovered_orders.get_recursively_not_directly_related()
+            all_invoices |= related_invoices | ii
+            all_orders |= related_orders | oo
+        return all_orders, all_invoices

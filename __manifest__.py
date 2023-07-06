9165b57c5ee17d304362bd508f82b8a9176f3b10
Sébastien Bühl <buhlsebastien@gmail.com>
2023-07-06 14:04:57 +0200

    [IMP] upgrade version to auto -u

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     'website': "http://www.buhl.be",
 
     'category': 'Customizations',
-    'version': '0.1.8.6',
+    'version': '0.1.8.7',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly

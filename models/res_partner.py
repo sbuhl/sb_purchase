0e41a9c75d7ef4d3e2cbf88580706abf92b1d7bf
Sébastien Bühl <buhlsebastien@gmail.com>
2023-07-06 14:44:15 +0200

    [FIX] partner add condition if empty

--- models/res_partner.py
+++ models/res_partner.py
@@ -18,21 +18,21 @@ class Partner(models.Model):
     def _check_rccm(self):
         pattern = r'^CD/[A-Z]{3}/RCCM/[0-9]{2}-[A-Z]{1}-[0-9]{5}$'
         for partner in self:
-            if not re.match(pattern, partner.rccm):
+            if partner.rccm and not re.match(pattern, partner.rccm):
                 raise ValidationError(_("RCCM number is not valid. It should respect the following: CD/YYY/RCCM/xx-Y-xxxxx (where x = number and Y = capitalised letter)"))
 
     @api.constrains('id_nat')
     def _check_id_nat(self):
         pattern = r'^[0-9]{2}-[A-Z]{1}[0-9]{4}-[A-Z]{1}[0-9]{5}[A-Z]{1}$'
         for partner in self:
-            if not re.match(pattern, partner.id_nat):
+            if partner.id_nat and not re.match(pattern, partner.id_nat):
                 raise ValidationError(_("Id. Nat. number is not valid. It should respect the following: xx-Yxxxx-YxxxxxY (where x = number and Y = capitalised letter)"))
 
     @api.constrains('nif')
     def _check_nif(self):
         pattern = r'^[A-Z]{1}[0-9]{7}[A-Z]{1}$'
         for partner in self:
-            if not re.match(pattern, partner.nif):
+            if partner.nif and not re.match(pattern, partner.nif):
                 raise ValidationError(_("NIF number is not valid. It should respect the following: YxxxxxxxY (where x = number and Y = capitalised letter)"))
 
     @api.model

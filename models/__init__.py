f77e139e48dd33fa12a53810afba5e7d424b2a6c
shortcut1337 <really_mad@hotmail.com>
2023-09-08 09:11:00 +0200

    [IMP] allow to add note on contact

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@
 from . import account_move
 from . import crm_lead
 from . import mail_template
+from . import mail_thread
 from . import project_task_recurrence
 from . import purchase_order
 from . import sale_order

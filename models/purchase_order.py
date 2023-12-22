0eb3fa0e4dd7ea470bbacd6705528b6741e25ec9
benjamin <benkisenge03@gmail.com>
2023-12-22 09:05:54 +0200

    Add new field in the purchase order

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -19,3 +19,11 @@ class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
     quantity_available_gse = fields.Float(related="product_id.qty_available")
+    sales_per_year = fields.Float(string="Sales Yearly")
+    COLOR_SELECTION = [
+        ('#FF0000', 'Red'),    # HTML color code for red
+        ('#00FF00', 'Green'),  # HTML color code for green
+        ('#0000FF', 'Blue'),
+    ]
+
+    quality_check = fields.Selection(COLOR_SELECTION, widget='color',string="Quality Check")

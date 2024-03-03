0266b5cac34ada2dd058cf15bec9d25064f73e9d
shortcut1337 <really_mad@hotmail.com>
2024-03-03 21:25:20 +0100

    wip

--- controllers/main.py
+++ controllers/main.py
@@ -5,10 +5,10 @@ import json
 
 from odoo import http, SUPERUSER_ID
 from odoo.http import request
-from odoo.addons.mail.controllers import discuss
+from odoo.addons.mail.controllers import attachment, thread
 
 
-class Website(discuss.DiscussController):
+class AttachmentController(attachment.AttachmentController):
 
     @http.route()
     def mail_attachment_upload(self, ufile, thread_id, thread_model, is_pending=False, **kwargs):
@@ -27,21 +27,22 @@ class Website(discuss.DiscussController):
         return request.make_json_response(vals)
 
     @http.route()
-    def mail_thread_messages(self, thread_model, thread_id, max_id=None, min_id=None, limit=30, **kwargs):
-        if thread_model == 'res.partner':
-            request.update_env(su=True)
-        res = super().mail_thread_messages(thread_model, thread_id, max_id, min_id, limit, **kwargs)
-
-        for resp in res:
-            for attach in resp.get('attachment_ids', []):
-                attach['accessToken'] = request.env['ir.attachment'].browse(attach['id']).access_token
-
-        return res
-
-    @http.route()
-    def mail_attachment_delete(self, attachment_id, access_token=None, **kwargs):
+    def mail_attachment_delete(self, attachment_id, access_token=None):
         if request.env['ir.attachment'].browse(int(attachment_id)).sudo().create_uid == request.env.user:
             request.update_env(su=True)
             request.update_env(user=SUPERUSER_ID)
-        return super().mail_attachment_delete(attachment_id, access_token, **kwargs)
-    
+        return super().mail_attachment_delete(attachment_id, access_token)
+
+
+class ThreadController(thread.ThreadController):
+    @http.route()
+    def mail_thread_messages(self, thread_model, thread_id, search_term=None, before=None, after=None, around=None, limit=30):
+        if thread_model == 'res.partner':
+            request.update_env(su=True)
+        res = super().mail_thread_messages(thread_model, thread_id, search_term=search_term, before=before, after=after, around=around, limit=limit)
+
+        for msg in res.get('messages'):
+            for attach in msg.get('attachments', []):
+                attach['accessToken'] = request.env['ir.attachment'].browse(attach['id']).access_token
+
+        return res
